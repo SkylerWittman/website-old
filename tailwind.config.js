9afe553eba37f9b83ec1759c7ca4fc08b872cff6
Skyler Wittman <skyler.wittman@gmail.com>
2021-12-23 15:21:43 -0500

    start work on tailwind rebuild

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./dist/**/*.{html,js}"],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem'
+        },
+        extend: {
+            gap: {
+                max: '42rem'
+            }
+        },
+    },
+    plugins: [],
+}
