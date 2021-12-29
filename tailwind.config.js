d0742512361cc68b28c850aa84154ea2ec236ca0
Skyler Wittman <skyler.wittman@gmail.com>
2021-12-29 16:51:47 -0500

    Style main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: ["./public/**/*.{html,js,ejs}"],
     theme: {
+        extend: {
+            colors: {
+                'terminal-green': '#282C34',
+                'text-grey': '#BEBEBE',
+                'lightspeed-red': '#D5372C',
+                'mnubo-blue': '#509EE3'
+            },
+            margin: {
+                'adjust': '25%',
+            }
+        },
         container: {
             center: true,
             padding: '2rem'
